4b94e722fc6015f5066520f363e5ed4d6dfdae33
Dennis Frett <dennis.frett@live.com>
2021-01-11 23:59:13 +0100

    Add documentation to header file.

--- include/nectransmitter.h
+++ include/nectransmitter.h
@@ -1,18 +1,21 @@
 #include <Arduino.h>
 
+/// Class that can send unmodulated NEC commands.
 class NECTransmitter {
 public:
+  /// Will set the given digital pin to OUTPUT and pull it to the non-active
+  /// value.
   NECTransmitter(uint8_t pin, bool activeLow = true);
 
+  /// Send given NEC command. Only the 8 address bits and the 8 command bits are
+  /// needed, the logical inverse is calculated automatically.
   void SendNEC(uint8_t address, uint8_t command) const;
 
 private:
-  // Get active value based on the active parameter and the active low / high
-  // member.
-  uint8_t GetActiveValue(bool active) const;
-
+  /// Send the given byte as NEC.
   void SendByte(uint8_t byte) const;
 
+  /// Send a 0 or 1 pulse. Value `true` means 1, `false` means 0.
   void SendPulse(bool value) const;
 
   // Digital output pin.
