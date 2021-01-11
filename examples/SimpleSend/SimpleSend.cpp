cfe6a9cc791e6911429a2a1aecd96d1caec25e93
Dennis Frett <dennis.frett@live.com>
2021-01-11 23:58:58 +0100

    Add examples.

---
+++ examples/SimpleSend/SimpleSend.cpp
@@ -0,0 +1,14 @@
+#include "nectransmitter.h"
+#include <Arduino.h>
+
+#define SEND_PIN 8
+
+NECTransmitter necTransmitter(SEND_PIN);
+
+void setup() {}
+
+void loop() {
+  // Will send 0x00FF 0xAB54.
+  necTransmitter.SendNEC(0x00, 0xAB);
+  delay(1000);
+}
